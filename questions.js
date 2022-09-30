1b371390da7ed4fd648ac611313b3866d222611b
Vansh29 <35563857+Vansh29@users.noreply.github.com>
2022-09-30 09:06:56 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Variable name in PHP starts with:",
+    answer: "$ (Dollar)",
+    options: [
+      "! (Exclamation)",
+      "$ (Dollar)",
+      "& (Ampersand)",
+      "# (Hash)"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is correct to add a comment in php?",
+    answer: "/* …… */",
+    options: [
+      "& …… &",
+      "# ...",
+      "/* …… */",
+      "// ……"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is the use of strlen() function in PHP?",
+    answer: "The strlen() function returns the length of string",
+    options: [
+      "The strlen() function returns the type of string",
+      "The strlen() function returns the value of string",
+      "The strlen() function returns both value and type of string",
+      "The strlen() function returns the length of string"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the use of strpos() function in PHP?",
+    answer: "The strpos() function is used to search for a character/text in a string",
+    options: [
+      "The strpos() function is used to search for the spaces in a string",
+      "The strpos() function is used to search for a number in a string",
+      "The strpos() function is used to search for a character/text in a string",
+      "The strpos() function is used to search for a capitalize character in a string"
+    ]
+  },
+];
