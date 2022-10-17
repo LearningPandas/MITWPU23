16c72a6ac1e20ea061762c895f2d17c292ea78a4
abyxixi <113407040+abyxixi@users.noreply.github.com>
2022-10-17 19:28:10 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,9 @@
+
+<link rel="stylesheet" href="upload.css">
+<form action="upload.php" method="POST">
+    <input type="file" multiple>
+    <p>Drag your files here or click here.</p>
+    <button type="submit">Upload</button>
+  </form>
+
+  <script src="/js/dashboard.js"></script>
