bb2d74e574d88fe12f515d63998a8e7af553d028
abyxixi <113407040+abyxixi@users.noreply.github.com>
2022-10-18 08:39:00 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang ="eng">
+    <head>
+        <title>LearningPandas</title>
+        <link rel="stylesheet" type = "text/css" href ="./css/homepage.css">
+        <link rel="icon" type="image/x-icon" href="./images/pngegg.png">
+    </head>
+    <body>
+        <header>
+            <div class="background">
+                <div class="layer">
+            <div class="main">
+                <div class="logo">
+                    <img src="./images/pngegg.png">
+                </div>
+                <ul>
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="aboutus.php">About Us</a> </li>
+                    <!-- <li><a href="#">Products</a> </li>    -->
+                    <li><a href="contact.php">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="first">
+                <h1>WELCOME TO</h1>
+            </div>
+            <div class="title">
+                <h2>LEARNING PANDAS</h2>
+            </div>
+            <div class="button">
+                <a href="learnerlogin.php" class="btn">Learner Login/SignUp</a>
+                <a href="instructorlogin.php" class="btn">Instructor Login/SignUp</a>
+            </div>
+            
+            
+        </header>
+    </body>
+</html>
